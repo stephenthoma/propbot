1d79f259dd002147c886cb586ff03eecab99c56f
stephenthoma <stephen@thoma.io>
2021-10-31 17:30:18 -0700

    Add script to pull + store the avg # of proposal voters

--- fodder/get_top_space_avg_voters.py
+++ fodder/get_top_space_avg_voters.py
@@ -1,1 +1,29 @@
-import gov-bot
+import json
+
+from govbot import snapshot, firestore
+
+
+spaceProposalsQuery = """
+query Proposals($id: String) {
+  proposals( where: {space: $id} ) { id }
+}
+"""
+
+
+with open("./all_spaces.json", "r") as spacefi:
+    space_json = json.load(spacefi)["data"]
+
+for space in space_json["spaces"]:
+
+    vars = {"id": space["id"]}
+    res = snapshot.run_query(spaceProposalsQuery, snapshot.SNAPSHOT_GRAPH_URL, variables=vars)
+    proposals = res["data"]["proposals"]
+    if len(proposals) < 3:
+        continue
+
+    prop_votes = [len(snapshot.get_votes(p["id"])) for p in proposals]
+    avg_votes = sum(prop_votes) / len(prop_votes)
+    if avg_votes != 0.0:
+        print(space["id"], avg_votes)
+        firestore.store_space_avg_voters(space["id"], avg_votes)
+        firestore.get_avg_space_voters(space["id"])
