c0ae9a9dffb2a2b516ea0f1c76561db1ef4bb9cc
stephenthoma <stephen@thoma.io>
2021-11-06 15:32:11 -0700

    Refactor twitter file to use sgqlc types

--- govbot/twitter.py
+++ govbot/twitter.py
@@ -1,32 +1,18 @@
 import os
 import datetime
+import typing
 import pytz
 
 import tweepy
 
 from govbot import snapshot
+from govbot.snapshot_schema import snapshot_schema as ss
 
 
 class GovTweeter:
     def __init__(self):
-        is_production = os.environ["GOVBOT_PRODUCTION"] == "true"
-        self.api = self._get_tweepy_api() if is_production else TweepyAPIMock()
-
-    def new_proposal_status(self, prop: dict) -> str:
-        """Create a string for a tweet about a new proposal"""
-        end_date_str = get_human_time(prop["end"])
-        url = snapshot.get_proposal_url(prop["space"]["id"], prop["id"])
-        name = get_space_name(prop)
-
-        return f"⚡️ {name} proposal: \"{prop['title']}\"\n\nVoting ends {end_date_str}\n{url}"
-
-    def contested_proposal_status(self, prop: dict) -> str:
-        """Create a string for a tweet about a contested proposal"""
-        end_date_str = get_human_time(prop["end"])
-        url = snapshot.get_proposal_url(prop["space"]["id"], prop["id"])
-        name = get_space_name(prop)
-
-        return f"⚔️ [contested] {name} proposal: \"{prop['title']}\"\n\nVoting ends soon {end_date_str}\n{url}"
+        self.is_production = os.environ["GOVBOT_PRODUCTION"] == "true"
+        self.api = self._get_tweepy_api()
 
     def _get_tweepy_api(self):
         CONSUMER_KEY = os.environ["CONSUMER_KEY"]
@@ -38,21 +24,35 @@ class GovTweeter:
         auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
         return tweepy.API(auth)
 
-    def update_twitter_status(self, status: str) -> dict:
-        """Post a tweet"""
-        result = self.api.update_status(status)
-        return result
+    def new_proposal_status(self, proposal: ss.Proposal) -> str:
+        """Create a string for a tweet about a new proposal"""
+        end_date_str = _get_human_time(proposal.end)
+        url = snapshot.get_proposal_url(proposal.space.id, proposal.id)
+        name = _get_space_name(proposal)
 
-    def get_recent_tweets(self, count=25):
-        return self.api.user_timeline(count=count)
+        return f'⚡️ {name} proposal: "{proposal.title}"\n\nVoting ends {end_date_str}\n{url}'
+
+    def contested_proposal_status(self, proposal: ss.Proposal) -> str:
+        """Create a string for a tweet about a contested proposal"""
+        end_date_str = _get_human_time(proposal.end)
+        url = snapshot.get_proposal_url(proposal.space.id, proposal.id)
+        name = _get_space_name(proposal)
+
+        return f'⚔️ [contested] {name} proposal: "{proposal.title}"\n\nVoting ends soon {end_date_str}\n{url}'
+
+    def update_twitter_status(self, status: str):
+        if self.is_production:
+            result = self.api.update_status(status)
+        else:
+            print(status)
 
     def has_recently_tweeted(self, search_str: str, cutoff_delta: datetime.timedelta) -> bool:
         cutoff_time = pytz.utc.localize(datetime.datetime.now() - cutoff_delta)
 
-        tweets = self.get_recent_tweets()
+        tweets = self.api.user_timeline(count=25)
         if tweets[-1].created_at > cutoff_time:
             # TODO: Improve this retrieval logic lol
-            tweets = self.get_recent_tweets(count=50)
+            tweets = self.api.user_timeline(count=100)
             assert tweets[-1].created_at <= cutoff_time
 
         included_tweets = [t for t in tweets if t.created_at >= cutoff_time]
@@ -60,19 +60,12 @@ class GovTweeter:
         return len(filtered_tweets) > 0
 
 
-def get_human_time(unix_time) -> str:
+def _get_human_time(unix_time: int) -> str:
     return datetime.datetime.fromtimestamp(unix_time).strftime("%H:%M %b %d %Y")
 
 
-def get_space_name(proposal: dict) -> str:
-    if proposal["space"]["twitter"]:
-        return f"@{proposal['space']['twitter']}"
+def _get_space_name(proposal: ss.Proposal) -> str:
+    if proposal.space.twitter:
+        return f"@{proposal.space.twitter}"
     else:
-        return proposal["space"]["name"]
-
-
-class TweepyAPIMock:
-    """Used to mock the Tweepy API instance in development. Prints to stdout instead of tweeting"""
-
-    def update_status(self, status):
-        print("[ DEBUG ]\n", status, "\n[ DEBUG ]")
+        return proposal.space.name
