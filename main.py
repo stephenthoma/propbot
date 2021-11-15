85bd894c1a6fdab44869a1f77a0068102a65fd27
stephenthoma <stephen@thoma.io>
2021-11-14 22:01:22 -0800

    Add cron for summary + high activity. Split cron logic based on message received

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 """
 IDEAS:
-    - weekly summary of # proposals accepted + denied, # voters overall. most active spaces?
     - high activity proposal alert tweet. when # voters is X std deviations above average
-    - need to make an allowlist of spaces so i can more confidently turn on auto tweets
     - consider adding cashtags to tweets?
     - make a mapping of space name to project twitter @
     - make a mapping of string to project twitter @ -- eg "Convex Finance" gets replaced with @convexfinance
-    - add circuit breaker to prevent @ tagging a project more than once a day
+PROBLEMS:
+    - has_already_tweeted_prop filter will prevent high activity / contested tweets for props that
+      were tweeted about at creation time
 """
 
 import json
+import base64
 from datetime import datetime
 
 from govbot import proposal_filters as pf
@@ -23,8 +24,8 @@ def webhook_entry(req):
     prop_id = req.json["id"].split("proposal/")[1]
     prop = snapshot.get_proposal(prop_id)
     if is_valid_proposal(prop):
-        govTweeter = GovTweeter()
-        govTweeter.tweet_proposal(prop)
+        gov_tweeter = GovTweeter()
+        gov_tweeter.tweet_proposal(prop)
 
     filters = [
         pf.is_old_proposal,
@@ -40,13 +41,19 @@ def webhook_entry(req):
 
 
 def cron_entry(event=None, context=None):
-    """Entrypoint for the pub-sub based cloud function
+    """Entrypoint for the pub-sub based cloud function"""
+    message = base64.b64decode(event["data"])
 
-    This cloud function will be triggered on a schedule. We will assume that it runs every 15 min.
-    without fail. This lets us ignore any proposals that were created more than 15 min. ago.
-    """
-    print(event, context)
-    govTweeter = GovTweeter()
+    if message == b"check_special":
+        contested_cron()
+        high_activity_cron()
+    elif message == b"summary":
+        gov_tweeter = GovTweeter()
+        gov_tweeter.update_twitter_status(gov_tweeter.weekly_summary_status())
+
+
+def contested_cron():
+    gov_tweeter = GovTweeter()
 
     ending_proposals = snapshot.get_ending_proposals()
     filters = [
@@ -61,16 +68,35 @@ def cron_entry(event=None, context=None):
     filtered_proposals = pf.apply_filters(filters, ending_proposals)
     for prop in filtered_proposals:
         if not firestore.has_contested_tweet(prop.id):
-            status = govTweeter.contested_proposal_status(prop)
-            govTweeter.update_twitter_status(status)
+            status = gov_tweeter.contested_proposal_status(prop)
+            gov_tweeter.update_twitter_status(status)
             firestore.store_contested_proposal_tweet(prop.id)
 
 
+def high_activity_cron():
+    gov_tweeter = GovTweeter()
+
+    ending_proposals = snapshot.get_ending_proposals()
+    filters = [
+        pf.has_blocked_words,
+        pf.is_blocked_space,
+        pf.is_low_follower_space,
+        pf.is_low_activity_proposal,
+        pf.has_recently_tweeted_space,
+        pf.has_already_tweeted_prop,
+    ]
+
+    filtered_proposals = pf.apply_filters(filters, ending_proposals)
+    for prop in filtered_proposals:
+        status = gov_tweeter.high_activity_proposal_status(prop)
+        gov_tweeter.update_twitter_status(status)
+
+
 def dev_get_new():
-    govTweeter = GovTweeter()
+    gov_tweeter = GovTweeter()
     new_proposals = snapshot.get_latest_proposals()
     filters = [
-        pf.is_old_proposal,
+        # pf.is_old_proposal,
         pf.has_blocked_words,
         pf.is_blocked_space,
         pf.is_low_follower_space,
@@ -80,10 +106,13 @@ def dev_get_new():
     proposals = pf.apply_filters(filters, new_proposals)
 
     for prop in proposals:
-        status = govTweeter.new_proposal_status(prop)
-        govTweeter.update_twitter_status(status)
+        status = gov_tweeter.new_proposal_status(prop)
+        gov_tweeter.update_twitter_status(status)
 
 
 if __name__ == "__main__":
     # cron_entry()
-    dev_get_new()
+    # dev_get_new()
+    high_activity_cron()
+    # gov_tweeter = GovTweeter()
+    # print(snapshot.get_week_summary())
